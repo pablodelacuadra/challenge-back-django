f04584425d5087fbee4c288ecb954b5923d974e1
pablodelacuadra <pablo.delacuadra@outlook.com>
2019-06-16 19:23:59 -0400

    ADD: not permission choose to serializer

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -16,14 +16,19 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         write_only=True,
     )
-    permission = PermissionSerializer()
+    permission = PermissionSerializer(
+        required=False,
+    )
 
     class Meta:
         model = User
         fields = ('name', 'last_names', 'username', 'password', 'permission')
 
     def create(self, validated_data):
-        permission_data = validated_data.pop('permission')
+        try:
+            permission_data = validated_data.pop('permission')
+        except:
+            permission_data = None
 
         django_user = UserDjango.objects.create(
             username=validated_data['username'],
@@ -34,8 +39,11 @@ class UserSerializer(serializers.ModelSerializer):
         django_user.set_password(validated_data['password'])
         django_user.save()
 
-        permission = Permission.objects.get(
-            name=permission_data['name'])
+        try:
+            permission = Permission.objects.get(
+                name=permission_data['name'])
+        except:
+            permission = None
 
         instance = User.objects.create(
             name=validated_data['name'],
