f309b6ef4d15ed31da21869725f3ae9c7cb2dda7
pablodelacuadra <pablo.delacuadra@outlook.com>
2019-06-16 02:39:05 -0400

    ADD: read only fields

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -8,7 +8,8 @@ class TicketSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Ticket
-        fields = ('title', 'description', 'status', 'created_at')
+        fields = ('id', 'title', 'description', 'status', 'created_at')
+        read_only_fields = ('id',)
 
     def get_created_at(self, obj):
         return obj.created_at.strftime("%Y-%m-%d %H:%M:%S")
